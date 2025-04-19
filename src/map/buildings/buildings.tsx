9d03896f1782c0fdad084da6e87535ecb6b2a4f1
jcampolim <julia.campolim@gmail.com>
2025-04-18 21:08:17 -0300

    Adding map

--- src/map/buildings/buildings.tsx
+++ src/map/buildings/buildings.tsx
@@ -1,14 +1,17 @@
 import { Polygon } from "react-leaflet";
-import mapa from "./mapa";
+import Mapa from "./mapa";
+import Colors from "./categories/mackenzie"
 
 export default function Buildings() {
-    const bounds: { bounds: [number, number][], color: string }[] = mapa;
+    const bounds: { bounds: [number, number][], name: string }[] = Mapa;
+    const colors = Colors;
 
     const buildings = bounds.map((item, index) => {
         return (
             <Polygon 
                 key={`${index}`}
-                color={item.color} 
+                color={colors.find(c => c.name === item.name)?.color}
+                fillOpacity={0.8}
                 positions={item.bounds} 
             />
         );
---
+++ src/map/buildings/categories/mackenzie.ts
@@ -0,0 +1,211 @@
+const educacao_basica = "#FB811D";
+const historia_tradicao_e_cultura = "#BDD634";
+const institucional = "#EF1827";
+const pionerismo_e_inovacao = "#027AC1";
+const predio_externo = "#6B2995"
+const tecnologia_e_saude = "#02B0CA";
+
+const Colors: { name: string, color: string }[] = [
+    {
+        color: educacao_basica,
+        name: "44",
+    },
+    {
+        color: educacao_basica,
+        name: "43",
+    },
+    {
+        color: educacao_basica,
+        name: "46",
+    },
+    {
+        color: educacao_basica,
+        name: "18",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "13",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "14",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "17",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "16",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "12",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "11",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "09",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "01",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "02",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "03",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "04",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "05",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "06",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "07",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "10",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "19",
+    },
+    {
+        color: historia_tradicao_e_cultura,
+        name: "20",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "23",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "24",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "25",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "31",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "33",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "37",
+    },
+    {
+        color: tecnologia_e_saude,
+        name: "38",
+    },
+    {
+        color: institucional,
+        name: "35",
+    },
+    {
+        color: institucional,
+        name: "40",
+    },
+    {
+        color: institucional,
+        name: "40",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "30",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "29",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "28",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "45",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "48",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "49",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "50",
+    },
+    {
+        color: pionerismo_e_inovacao,
+        name: "52",
+    },
+    {
+        color: predio_externo,
+        name: "E85",
+    },
+    {
+        color: predio_externo,
+        name: "E143",
+    },
+    {
+        color: predio_externo,
+        name: "E181",
+    },
+    {
+        color: predio_externo,
+        name: "E993",
+    },
+    {
+        color: predio_externo,
+        name: "E101",
+    },
+    {
+        color: predio_externo,
+        name: "E249",
+    },
+    {
+        color: predio_externo,
+        name: "E163",
+    },
+    {
+        color: predio_externo,
+        name: "E139",
+    },
+    {
+        color: predio_externo,
+        name: "E117",
+    },
+    {
+        color: predio_externo,
+        name: "E40",
+    },
+    {
+        color: predio_externo,
+        name: "E44",
+    },
+];
+
+export default Colors;
